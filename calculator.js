9bfa64a7c9b7edc49183acffdc5ba55558717ff4
Yash Sinha <yashsinha7696@gmail.com>
2020-10-25 21:20:11 +0530

    BMI Calculator code added on the server.

--- calculator.js
+++ calculator.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
+//Basic Addition Calculator
 app.get("/", function (request, response) {
     response.sendFile(__dirname+"/index.html");
 });
@@ -15,6 +16,20 @@ app.post("/", function (request, response) {
     var result = num1 + num2;
 
     response.send("The result of the calculation is: " + result);
+})
+
+//BMI Calculator
+app.get("/bmiCalculator", function (request, response) {
+    response.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmiCalculator", function (request, response) {
+    var weight = Number(request.body.weight);
+    var height = Number(request.body.height);
+
+    var bmi = weight / (height * height);
+
+    response.send("The BMI is: " + bmi);
 })
 
 app.listen(3000, function () {
